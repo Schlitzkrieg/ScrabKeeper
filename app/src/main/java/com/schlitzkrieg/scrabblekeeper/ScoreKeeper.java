511eb61dc124ed92e07eec97952bf6acbf727fd2
jSchlichting <7680545+Schlitzkrieg@users.noreply.github.com>
2018-02-24 16:43:50 -0500

    Created ScoreKeeper.java class

---
+++ app/src/main/java/com/schlitzkrieg/scrabblekeeper/ScoreKeeper.java
@@ -0,0 +1,23 @@
+package com.schlitzkrieg.scrabblekeeper;
+
+/**
+ * Created by jdsch on 2/24/2018.
+ */
+
+public class ScoreKeeper {
+
+    int Player1Score;
+    int Player2Score;
+    int Player3Score;
+    int Player4Score;
+
+    ScoreKeeper(int p1s, int p2s, int p3s, int p4s){
+
+        this.Player1Score = p1s;
+        this.Player2Score = p2s;
+        this.Player3Score = p3s;
+        this.Player4Score = p4s;
+    }
+
+
+}
